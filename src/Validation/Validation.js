d7cc7ce59b844f2de44617c126082e36abdb1e62
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-08 16:14:09 +0100

    changed validation method text

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -5,8 +5,8 @@ const validation = (props) =>{
     return(
         <div className="Validation grid-column-12">
             Is valid: {props.isValid ? 
-            <span className="text-green">true</span> :
-            <span className="text-red">false</span>}
+            <span className="text-green">Text long enough</span> :
+            <span className="text-red">Text too short</span>}
         </div>
     )
 }
